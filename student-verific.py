0bea9350da0a9d2009a01303900a859199825068
killshott3r <82105252+killshott3r@users.noreply.github.com>
2022-03-20 09:32:43 +0000

    Add files via upload

--- student-verific.py
+++ student-verific.py
@@ -1,36 +1,38 @@
 import numpy as np
-import qrcode
 import cv2
 from pyzbar.pyzbar import decode
-from openpyxl import Workbook
 
-wb = Workbook()
-ws = wb.active
+# variables blah blah
 
 myfile = open('attendence.csv', 'a+')
 students = [] # TYPE OUT YOUR OWN NAMES HERE LIKE SO: ['name1', 'name2']
 names = []
-print(len(students))
+
+# vid capture
+
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
+
+# main stuff
 
 while True:
     ret, frame = cap.read()
     
     for barcode in decode(frame):
-        print(barcode.data)
-        myData = barcode.data.decode('utf-8')
-        
+        myData = barcode.data.decode('utf-8') # decode barcode seen in captured video
         print(myData)
+
+        # outputting a quadrilateral around the qrcode (just for show, doesnt do alot)
+
         pts = np.array([barcode.polygon], np.int32)
         cv2.polylines(frame,[pts],True,(255,0,0),5)
         pts2 = barcode.rect
         cv2.putText(frame,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
+
         string = f'{myData},Present\n'
-        
         if string not in names:
-            names.append(string)
+            names.append(string) 
     
     cv2.imshow('cam',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -39,8 +41,8 @@ while True:
         for i in range(len(students)):
             string = f'{students[a]},Present\n'
             if  string not in names:
-                myfile.write(f'{students[a]},Absent\n')
+                myfile.write(f'{students[a]},Absent\n') # if student name not in present name list, then absent
             elif string in names:
-                myfile.write(f'{students[a]},Present\n')
+                myfile.write(f'{students[a]},Present\n') # if student name in present name list, then present
             a += 1
         break
